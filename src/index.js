e44f2fa39d2c603d868a872e5363f39cb5b7e464
Jose Soto <jsoto7087@gmail.com>
2022-05-04 20:23:27 -0400

    Completed Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// target targets the event children the child of the form and index for siblims-- value for contenct.
+// https://github.com/jsoto007/phase-1-js-fetch-on-demand-with-forms
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userInput = e.target.children[1].value // here we are targetting the form. then the childrean from there we get an array
+    const p = document.createElement('p')
+    const ul = document.getElementById('tasks')
+    ul.appendChild(p);
+    p.innerText = userInput;
+    form.reset()
+})
+// 
